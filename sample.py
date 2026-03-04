97d63838075bd094e0981c6a8f40b0d9df26bcdd
unknown <shelsiarg04@gmail.com>
2026-03-04 18:39:08 +0530

    Uploading chnages

---
+++ sample.py
@@ -0,0 +1,14 @@
+print("Welcome to Python programming!")
+username = "Shelsia"
+userID = 12
+is_active = True
+value = 3.14
+print(type(username))
+print(type(userID))     
+print(type(is_active))   
+print(type(value))
+
+print(username)
+print(userID)
+print(is_active)
+print(value)
